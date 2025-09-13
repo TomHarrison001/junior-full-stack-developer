ef34d161f288eb71ac5d39328e88610546000902
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-09-13 16:49:31 +0100

    Update navigation bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,17 +7,27 @@ const Header = () => {
             <nav class="navbar">
                 <div class="container">
                     <h2>Company Name</h2>
-                    <ul class="nav-list">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/register">Register</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/login">Login</a>
-                        </li>
-                    </ul>
+                    <div className="lists">
+                        <ul class="nav-list">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/about">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/contact">Contact Us</a>
+                            </li>
+                        </ul>
+                        <ul class="nav-list">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/register">Register</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/login">Login</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </div>
