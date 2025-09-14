3bcec598ee909f0a7de4c3c0b78bf587b9ef8771
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-09-14 09:52:02 +0100

    Create user model, controller and routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { connectDB } from "./database.js";
+import router from "./user/user.routes.js";
 
 const app = express();
 const PORT = 5000;
 
 // Use JSON parsing middleware and user routes
 app.use(express.json());
+app.use("/users", router);
 
 app.listen(PORT, () => {
   connectDB();
---
+++ server/user/user.controller.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+import User from "./user.model.js";
+
+// GET request: retrieve all users
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    console.log("Error fetching users: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error." });
+  }
+};
+
+// GET by specific _id request: retrieve a single user
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+
+  // check user exists
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "User not found." });
+  }
+
+  try {
+    const user = await User.findById(id);
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    console.log("Error fetching user: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error." });
+  }
+};
+
+// POST request: create a new user
+export const createUser = async (req, res) => {
+  const user = req.body;
+
+  // check for empty fields
+  if (!user.username || !user.email || !user.password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields." });
+  }
+
+  const newUser = new User(user);
+
+  try {
+    await newUser.save();
+    res.status(201).json({ success: true, data: newUser });
+  } catch (error) {
+    console.error("Error creating new user:", error.message);
+    res.status(500).json({ success: false, message: "Server Error." });
+  }
+};
+
+// PUT request: update the details of a user by id
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+
+  // check user exists
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Error: User not found." });
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, user, { new: true });
+    res.status(200).json({ success: true, data: updatedUser });
+  } catch (error) {
+    console.error("Error updating user:", error.message);
+    res.status(500).json({ success: false, message: "Server Error." });
+  }
+};
+
+// DELETE request: delete a user by id
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  // check user exists
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Error: User not found." });
+  }
+
+  try {
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "User deleted." });
+  } catch (error) {
+    console.error("Error deleting user:", error.message);
+    res.status(500).json({ success: false, message: "Server Error." });
+  }
+};
